7dfcc13397cb932c6097b5aa80ed67987a22f770
DL-SkyPlusOnline@bskyb.com <telly@bskyb.com>
2015-11-02 17:01:29 +0000

    initial commit of npm ng2 app without proper wiring

---
+++ src/app/app.ts
@@ -0,0 +1,10 @@
+import {Component, bootstrap} from 'angular2/angular2';
+
+@Component({
+    selector: 'strapline',
+    template: '<p class="title">Daily Developer Dashboard</p>'
+})
+
+class AppComponent { }
+
+bootstrap(AppComponent);
