5d9bbb98d0aae3cf508281219bc9eefb1f7a33ad
Ben McGeachie <info@krakendigit.al>
2015-11-03 08:57:54 +0000

    removing alpha channels from model

--- src/app/components/design-container.ts
+++ src/app/components/design-container.ts
@@ -2,9 +2,9 @@ import {Component, NgFor} from 'angular2/angular2';
 import {Dee} from './d';
 
 var Deez: Dee[] = [
-  { color: 'rgba(13, 71, 161, 1)' },
-  { color: 'rgba(25, 118, 210, 1)' },
-  { color: 'rgba(33, 150, 243, 1)' }
+  { color: 'rgb(13, 71, 161)' },
+  { color: 'rgb(25, 118, 210)' },
+  { color: 'rgb(33, 150, 243)' }
 ];
 
 @Component({
@@ -13,7 +13,7 @@ var Deez: Dee[] = [
   styleUrls: ['app/styles/design.css'],
   template: `
     <div class="container">
-      <div *ng-for="#layer of deez" class="dee" [style.background]="layer.color"></div>
+      <div *ng-for="#layer of deez" class="dee" [style.background]="layer.color" [title]="layer.color"></div>
       <div class="dee mask" [style.background]="background"></div>
     </div>
   `
