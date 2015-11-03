8bb424affcbdc4b6353dc8c5ddeddaad666d3f03
Ben McGeachie <info@krakendigit.al>
2015-11-03 00:19:38 +0000

    moving to rgb colouring and :nth-child based CSS selectors

--- src/app/components/design-container.ts
+++ src/app/components/design-container.ts
@@ -2,9 +2,9 @@ import {Component, NgFor} from 'angular2/angular2';
 import {Dee} from './d';
 
 var Deez: Dee[] = [
-  { klass: 'outer', color: '#0D47A1' },
-  { klass: 'middle', color: '#1976D2' },
-  { klass: 'inner', color: '#2196F3' }
+  { color: 'rgba(13, 71, 161, 1)' },
+  { color: 'rgba(25, 118, 210, 1)' },
+  { color: 'rgba(33, 150, 243, 1)' }
 ];
 
 @Component({
@@ -13,7 +13,7 @@ var Deez: Dee[] = [
   styleUrls: ['app/styles/design.css'],
   template: `
     <div class="container">
-      <div *ng-for="#layer of deez" class="dee {{layer.klass}}" [style.background]="layer.color"></div>
+      <div *ng-for="#layer of deez" class="dee" [style.background]="layer.color"></div>
       <div class="dee mask" [style.background]="background"></div>
     </div>
   `
